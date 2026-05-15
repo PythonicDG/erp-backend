551879b2904dae37aec01e96ddd71b3fd1c8e68b
PythonicDG <dipakgaikwadmg@gmail.com>
2026-05-15 16:45:00 +0530

    [fix] static files issues

--- core/settings.py
+++ core/settings.py
@@ -209,3 +209,4 @@ MEDIA_ROOT = BASE_DIR / 'company_logos'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
